8fe20684718304c0133f337a8b89f57b35897c24
Neel028 <neelkoshiya028@gmail.com>
2023-04-07 21:49:55 +0530

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
 
 function App() {
+  const [state, setState] = useState(0)
+
+  {document.title=`${state} s`}
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={()=>setState(state+1)}>click {state}</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
